cb4bb8805c2b3ccf4fda7e344d6768f427524d55
ahmad1212as <ahmadweb2008@gmail.com>
2025-10-13 07:22:45 -0700

    Create Search information.js

---
+++ School/Search information.js
@@ -0,0 +1,30 @@
+[
+  {
+    "name": "مادة الأحياء",
+    "link": "https://chat.whatsapp.com/Hq2W9SvlW9pIGMMVDSR64q",
+    "date": "2025-10-01",
+    "class": "الفصل الأول",
+    "unit": "الوحدة الأولى"
+  },
+  {
+    "name": "مادة التربية الإسلامية",
+    "link": "https://chat.whatsapp.com/BzpgM0U623VADINErOB5Ok",
+    "date": "2025-10-05",
+    "class": "الفصل الأول",
+    "unit": "الوحدة الثانية"
+  },
+  {
+    "name": "مادة الرياضيات",
+    "link": "https://chat.whatsapp.com/DRDcqi7hmXnKVHXCcsvoAr",
+    "date": "2025-10-03",
+    "class": "الفصل الثاني",
+    "unit": "الوحدة الأولى"
+  },
+  {
+    "name": "مادة الفيزياء",
+    "link": "https://chat.whatsapp.com/CRSBgze7xxPL8ihAaxM1Gf",
+    "date": "2025-10-06",
+    "class": "الفصل الثاني",
+    "unit": "الوحدة الثانية"
+  }
+]
